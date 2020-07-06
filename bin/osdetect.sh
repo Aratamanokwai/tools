d5e13fe90806e70e49e77552e80138734371c537
ozawa3 <58377611+ozawa3-labo@users.noreply.github.com>
2020-07-06 20:09:39 +0900

    ADD: bin/

---
+++ bin/osdetect.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -
+#
+# Cybersecurity Ops with bash
+# osdetect.sh
+#
+# Description:
+# Distinguish between MS-Windows/Linux/MacOS
+#
+# Usage: bash osdetect.sh
+#   output will be one of: Linux MSWin macOS
+#
+
+if type -t wevtutil &> /dev/null
+then
+    OS=MSWin
+elif type -t scutil &> /dev/null
+then
+    OS=MacOS
+else
+    OS=Linux
+fi
+
+echo $OS
