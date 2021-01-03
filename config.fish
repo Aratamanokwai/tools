8d74fe97fabec2b4f2adb9f047a83deb2611fd00
ozawa3 <58377611+ozawa3-labo@users.noreply.github.com>
2021-01-03 23:20:51 +0900

    ADD: fish config

---
+++ config.fish
@@ -0,0 +1,14 @@
+# some more ls aliases
+abbr --add ll 'ls -l'
+abbr --add la 'ls -A'
+abbr --add lF 'ls -CF'
+abbr --add md 'mkdir'
+
+abbr --add bb 'byobu'
+abbr --add vi 'vim'
+abbr --add py3 'python3'
+abbr --add ezo 'pushd /mnt/c/Users/1234/Documents/GitHub/py3/ezo'
+
+abbr --add pl 'pylint'
+abbr --add fl 'flake8'
+abbr --add pds 'pydocstyle'
